e156b34c0f551be936542dfd95adeadb287be4c2
Khaled Elwan <khaledelwan2004@gmail.com>
2026-08-11 15:41:34 +0300

    lab1 done

---
+++ Laps/lap1/lap1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void main()
+{
+	//lab1
+	
+	//printf("I'm Kahled \nMy BIrth date is 12 jun\nI am sudent at Alex Uni\n") ;
+	
+	//lab2
+	
+	/*int a,b ;
+	
+	printf("enter the num1: ") ; 
+	scanf("%i",&a) ;
+	printf("enter the num2: ") ; 
+	scanf("%i",&b) ;
+	printf("a+b=%d\n",a+b) ;	
+	printf("a*b=%d\n",a*b) ;*/
+	
+	//lab3
+	
+	/*int x=15,y=20 ;
+	printf("%d\n",x&y) ;
+	
+	printf("%d\n",x|y) ;
+	
+	printf("%d\n",x>>3) ;
+	
+	printf("%d\n",y<<4) ;*/
+	
+}
