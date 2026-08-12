5bdabced0db942d4d2dedaa5fc2f21d34820011d
Khaled Elwan <khaledelwan2004@gmail.com>
2026-08-12 05:58:15 +0300

    added Assignment1

---
+++ Assignments/S1/prog2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void main()
+{
+	
+	int num1,num2,num3 ;
+	
+	printf("Please enter number 1:") ;
+	
+	scanf("%i" ,&num1) ;
+	
+	printf("Please enter number 2:") ;
+	
+	scanf("%i" ,&num2) ;
+	
+	printf("Please enter number 3:") ;
+	
+	scanf("%i" ,&num3) ;
+	
+	printf("number 3:%d\
+		   \nnumber 2:%d\
+		   \nnumber 1:%d \n",num3,num2,num1) ;
+		   
+	
+
+}
---
+++ Assignments/S1/prog3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void main()
+{
+	
+	int a,b ;
+	
+	printf("Please enter a:") ;
+	
+	scanf("%i" ,&a) ;
+	
+	printf("Please enter b:") ;
+	
+	scanf("%i" ,&b) ;
+	
+	printf("a + b =%d\n",a+b) ;
+	
+	printf("a - b =%d\n",a-b) ;
+	
+	printf("a & b =%d\n",a&b) ;
+	
+	printf("a | b =%d\n",a|b) ;
+	
+	printf("a ^ b =%d\n",a^b) ;
+
+
+}
